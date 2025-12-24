3056919a344a7ed9dd94704da6a6325597b21d68
Thanwarat2347 <Thanwarat.boon@ku.th>
2025-12-24 21:57:59 +0700

    add folder backend and fronend

--- ิbackend/server.js
+++ ิbackend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server ทำงานแล้ว!");
+  res.send("้hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
 });
 
 app.listen(3001, () => {
